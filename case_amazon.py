247c471a3dc6a7bae8211d8c4557bc34999e04e5
golnazir <golnazirannejad@gmail.com>
2019-03-04 11:18:08 -0800

    Refactored case_amazon to have one djl object per wave.

--- case_amazon.py
+++ case_amazon.py
@@ -10,8 +10,6 @@ from scipy import interpolate
 from DJL import DJL, Diagnostic, plot
 import matplotlib.pyplot as plt
 
-import scipy.io as sio #todo: delete this
-
 # Load the Amazon data
 data     = numpy.genfromtxt('case_amazon.txt', delimiter=',') #columns are z,rho,u
 zdata    = data[:, 0]
@@ -24,7 +22,7 @@ uzdata    = numpy.gradient(udata,zdata)
 uzzdata   = numpy.gradient(uzdata,zdata)
 
 # Density adjustments (to ensure that drho/dz is negative)
-rhodata     = numpy.copy(rhodata0)              # copy original density
+rhodata     = numpy.copy(rhodata0)  # copy original density
 rhodata[-1] = rhodata[-2]*0.9999    # replace surface value to ensure positive N^2
 
 tmp = []
@@ -60,58 +58,46 @@ rhoz   = lambda z: interpolate.interp1d(zdata,rhozdata,fill_value="extrapolate")
 fUbg   = lambda z: interpolate.interp1d(zdata,udata   ,fill_value="extrapolate")(z)
 fUbgz  = lambda z: interpolate.interp1d(zdata,uzdata  ,fill_value="extrapolate")(z)
 fUbgzz = lambda z: interpolate.interp1d(zdata,uzzdata ,fill_value="extrapolate")(z)
-
+breakpoint()
 # Now we have data, prepare for DJLES
-L       = 600   # domain width (m)
-H       = 80    # domain depth (m)
-rho0    = 1000  # reference density (kg/m^3)
+L, H = 600, 80   # domain width (m) and depth (m)
+rho0 = 1000      # reference density (kg/m^3)
 #verbose = 1;
 
 start_time = time.time()
 
 # Start at low resolution/epsilon, no background velocity, and raise amplitude
-NX = 32
-NZ = 32
-A1 = 1e5
-A2 = 1e6
+NX, NZ = 32 , 32
+A1, A2 = 1e5, 1e6
+for ii, A in enumerate (numpy.linspace(A1, A2, 3)):
+    if ii == 0:
+        djl = DJL(A, L, H, NX, NZ, rho, rhoz, rho0 = rho0, epsilon=1e-3)
+    else:
+        djl = DJL(A, L, H, NX, NZ, rho, rhoz, rho0 = rho0, epsilon=1e-3, initial_guess = djl)
 
-djl = DJL(A1, L, H, NX, NZ, rho, rhoz, rho0 = rho0)
-
-djl.refine_solution(epsilon=1e-3)
-
-for A in numpy.linspace(550000, A2, 2):
-    djl.A = A
-    print('**A = ' , A, ' , ', djl.A)
-    djl.refine_solution(epsilon=1e-3)
-    
 # Improve resolution, raise amplitude
-djl.change_resolution(64, 64) #NX=64; NZ=64;
-A1=1e6
-A2=5e6
+NX, NZ = 64 , 64
+A1, A2 = 1e6, 5e6
 for A in numpy.linspace(A1, A2, 3):
-    djl.A = A
-    djl.refine_solution(epsilon=1e-3)
-    
+    djl = DJL(A, L, H, NX, NZ, rho, rhoz, rho0 = rho0, epsilon=1e-3, initial_guess = djl)
+
 # Now bring in the background velocity incrementally
 for alpha in numpy.linspace(0.25,1,4):
     # Velocity profile for this wave
-    djl.Ubg  = lambda z: alpha*fUbg(z)
-    djl.Ubgz = lambda z: alpha*fUbgz(z)
-    djl.Ubgzz= lambda z: alpha*fUbgzz(z)
-
-    djl.refine_solution(epsilon=1e-3)
+    Ubg  = lambda z: alpha*fUbg(z)
+    Ubgz = lambda z: alpha*fUbgz(z)
+    Ubgzz= lambda z: alpha*fUbgzz(z)
+    djl = DJL(A, L, H, NX, NZ, rho, rhoz, rho0 = rho0, Ubg = Ubg, Ubgz= Ubgz, Ubgzz = Ubgzz, epsilon=1e-3, initial_guess = djl)
 
 # Improve resolution and epsilon, raise amplitude
-djl.change_resolution(128, 128) #NX=128; NZ=128; 
-A1=5e6
-A2=7.5e6
+NX, NZ = 128, 128
+A1, A2 = 5e6, 7.5e6
 for A in numpy.linspace(A1, A2, 4):
-    djl.A = A
-    djl.refine_solution(epsilon=1e-4, relax=0.4)
+    djl = DJL(A, L, H, NX, NZ, rho, rhoz, rho0 = rho0, Ubg = Ubg, Ubgz= Ubgz, Ubgzz = Ubgzz, relax=0.4, epsilon=1e-4, initial_guess = djl)
 
 # Final wave: increase resolution, iterate to convergence
-djl.change_resolution(256, 256)     #NX=256; NZ=256; 
-djl.refine_solution(epsilon=1e-6, relax=0.4)
+NX, NZ = 256, 256
+djl = DJL(A, L, H, NX, NZ, rho, rhoz, rho0 = rho0, Ubg = Ubg, Ubgz= Ubgz, Ubgzz = Ubgzz, relax=0.4, epsilon=1e-6, initial_guess = djl)
 
 # Compute diagnostics, plot wave
 diag = Diagnostic(djl)
